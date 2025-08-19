0658bd056ed77fc4bccee1217a127b4b20926844
Jorge Fernando Soares Remete <jorgeodestruidor@gmail.com>
2025-08-19 09:14:51 -0300

    Started the script for switching images

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const items = document.querySelectorAll('.item');
+const dots = document.querySelectorAll('.dot');
+const numberIndicator = document.querySelector('.numbers');
+const list = document.querySelector('.list');
+
+
+
+let active = 0;
+const total = items.length;
+let timer;
+
+
+function update (direction)
+{
+  document.querySelector('.item.active').classList.remove('active');
+  document.querySelector('.dot.active').classList.remove('active');
+}
+
+prevButton.addEventListener('click', () => {
+    updated(-1);
+})
+nextButton.addEventListener('click', () => {
+   update(1);
+})
